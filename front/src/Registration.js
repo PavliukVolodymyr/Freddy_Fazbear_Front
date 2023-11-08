905ba405d1e9178f7e5221781f6f8edcd4992449
sashadioma <102321331+sashadioma@users.noreply.github.com>
2023-11-08 17:34:47 +0200

    css changes registration

--- front/src/Registration.js
+++ front/src/Registration.js
@@ -43,10 +43,22 @@ const Registration = () =>{
                      
                      ></input>
                 </div>
-
+                <div class="passwords-must-be-at-least-6-characters">
+  Passwords must be at least 6 characters.
+</div>
                <div className="rectangle-202020">
                 <button className="continue1">Continue</button>
                </div> 
+               <div class="frame-21">
+  <div class="rectangle-22"></div>
+  <div class="rectangle-23"></div>
+  <div class="already-have-an-account">Already have an account?</div>
+  <div class="group-63">
+    <div class="rectangle-20"></div>
+    <div class="sing-in">Sing in</div>
+  </div>
+</div>
+
 
           </div>
           
