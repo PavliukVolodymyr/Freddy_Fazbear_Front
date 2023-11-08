f83d41f17dfc51ed7048f4db5ec5646a2f8712c4
sashadioma <102321331+sashadioma@users.noreply.github.com>
2023-11-08 17:20:20 +0200

    some css changes

--- front/src/componets/CountSelector.jsx
+++ front/src/componets/CountSelector.jsx
@@ -14,16 +14,16 @@ function CountSelector({ initialValue, onQuantityChange }) {
         <div className="select-container">
 
             <select className="select-element" value={quantity} onChange={handleQuantityChange}>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
-                <option value={10}>10</option>
+                <option className="value" value={1}>1</option>
+                <option className="value" value={2}>2</option>
+                <option className="value" value={3}>3</option>
+                <option className="value" value={4}>4</option>
+                <option className="value" value={5}>5</option>
+                <option className="value" value={6}>6</option>
+                <option className="value" value={7}>7</option>
+                <option className="value" value={8}>8</option>
+                <option className="value" value={9}>9</option>
+                <option className="value" value={10}>10</option>
                 {/* Додайте інші значення, які вам потрібні */}
             </select>
             <label className="select-label">
